a4c7184287c5ea14bf93734ee4f94299464f10fb
Lindakat <lindakt16@gmail.com>
2021-04-21 17:22:09 -0500

    Netlify function fully working! Query cleaned up

--- netlify/functions/githubApiCall.js
+++ netlify/functions/githubApiCall.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 
-const gqlQuery = `query($query: String!) {
-  search(query: $query, type: USER, first:9) {
+const gqlQuery = (type) => `query($query: String!) {
+  search(query: $query, type: USER, ${type}) {
     userCount
     pageInfo {
       startCursor
@@ -60,6 +60,8 @@ exports.handler = async function (event) {
     query: event.queryStringParameters.q
   }
 
+  const type = event.queryStringParameters.type;
+
   const endpoint = 'https://api.github.com/graphql';
   
   const options = {
@@ -68,164 +70,31 @@ exports.handler = async function (event) {
       "Content-Type": "application/json",
       "Authorization": `Bearer ${process.env.TOKEN}`
     },
-    body: JSON.stringify({ query: gqlQuery, variables })
+    body: JSON.stringify({ query: gqlQuery(type), variables })
   };
 
   const userData = await fetch(endpoint, options)
     .then(res => res.json())
     .then(users => {
-      users.data.search
-    })
+      const fullData = users.data.search;
+      const overview = {
+        userCount: fullData.userCount,
+        pageInfo: {...fullData.pageInfo}
+      }
 
-  console.log(userData);
+      const usersInfo = users.data.search.edges.map(user => user.node);
+
+      return {overview, usersInfo};
+    })
+    .catch(err => console.error(err))
+
+  // console.log(userData);
 
   return {
     statusCode: 200,
-    body: JSON.stringify(userData)
+    body: JSON.stringify(userData),
   }
 }
-
-
-
-
-
-// 
-    // clear any existing data, if another search had been done previously
-    // this.users = {},
-    // this.noUsers = false;
-    // this.userCount = 0;
-    // this.pageInfo = {
-    //   prev: false,
-    //   next: false,
-    //   firstUser: '',
-    //   lastUser: '',
-    // };
-
-    // const info = userData.data.search;
-    // // logic so if no results return, user will see informational data
-    // if (info.userCount === 0) {
-    //   this.noUsers = true;
-    //   return;
-    // }
-
-    // const pageData = info.pageInfo;
-    // this.pageInfo.prev = pageData.hasPreviousPage;
-    // this.pageInfo.next = pageData.hasNextPage;
-    // this.pageInfo.firstUser = pageData.startCursor;
-    // this.pageInfo.lastUser = pageData.endCursor;
-
-    // // storing user results in a variable, then filtering it to remove anyone who has a name but nothing else
-    // const results = userData.data.search.edges;
-    // const legitUsers = results.filter(user => {
-    //   const userObj = user.node;
-    //   return Object.keys(userObj).length > 0;
-    // });
-    // this.userCount = info.userCount - legitUsers.length;
-    // this.loading = false;
-    // this.users = legitUsers;
-  // })
-  // .catch(err => console.error(err))
-
-  // const backQuery = `query($query: String!) {
-  //   search(query: $query, type: USER, last: 9, before: "${cursor}") {`
-  // //if next clicked, gets first 9 results after the last cursor
-  // const fwdQuery = `query($query: String!) {
-  //   search(query: $query, type: USER, first: 9, after: "${cursor}") {`
   
   // const gqlQuery = `${direction === "forward" ? fwdQuery : backQuery}
-  //     userCount
-  //     pageInfo {
-  //       startCursor
-  //       endCursor
-  //       hasNextPage
-  //       hasPreviousPage
-  //     }
-  //     edges {
-  //       cursor
-  //       node {
-  //         ... on User {
-  //           name
-  //           login
-  //           bio
-  //           url
-  //           avatarUrl
-  //           isHireable
-  //           followers {
-  //             totalCount
-  //           }
-  //           starredRepositories {
-  //             totalCount
-  //           }
-  //           pinnedItems(first: 3) {
-  //             edges {
-  //               node {
-  //                 ... on Repository {
-  //                   name
-  //                   description
-  //                   url
-  //                   languages(first:3, orderBy: {field: SIZE, direction: DESC} ) {
-  //                     edges {
-  //                       node {
-  //                         name
-  //                       }
-  //                     }
-  //                   }
-  //                 }
-  //                 ... on Gist {
-  //                   name
-  //                   description
-  //                   url
-  //                 }
-  //               }
-  //             }
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  // }`
-
-  // const variables = {
-  //   query: this.queryText,
-  // };
-
-  // const endpoint = 'https://api.github.com/graphql';
-
-  // const options = {
-  //   method: "POST",
-  //   headers: { 
-  //     "Content-Type": "application/json",
-  //     "Authorization": `Bearer ${process.env.TOKEN}`
-  //   },
-  //   body: JSON.stringify({ query: gqlQuery, variables })
-  // };
-
-  // fetch(endpoint, options)
-  //   .then(res => res.json())
-  //   .then(userData => {
-  //     // clear any existing data so new results can go in (just to be sure there's nothing mixed)
-  //     this.users = {},
-  //     this.userCount = 0;
-  //     this.pageInfo = {
-  //       prev: false,
-  //       next: false,
-  //       firstUser: '',
-  //       lastUser: '',
-  //     };
-
-  //     const pageData = userData.data.search.pageInfo;
-  //     this.pageInfo.prev = pageData.hasPreviousPage;
-  //     this.pageInfo.next = pageData.hasNextPage;
-  //     this.pageInfo.firstUser = pageData.startCursor;
-  //     this.pageInfo.lastUser = pageData.endCursor;
-
-  //     const results = userData.data.search.edges;
-  //     const legitUsers = results.filter(user => {
-  //       const userObj = user.node;
-  //       return Object.keys(userObj).length > 0;
-  //     });
-  //     this.userCount = userData.data.search.userCount - legitUsers.length;
-  //     this.loading = false;
-  //     this.users = legitUsers;
-  //   })
-  //   .catch(err => console.error(err));
+  //   
--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
     <section class="search-section">
       <Label for="search-bar" class="search-bar-label"> <img src="../static/icons/search.svg" class="icons"> Who would you like to search for?</Label>
       <div class="search-wrapper">
-        <input type="text" name="search-bar" class="search" placeholder="GitHub user name" v-model="queryText" v-on:keypress.enter="getInitData()">
-        <button class="search-button" type="button" v-on:click="getInitData()">Search</button>
+        <input type="text" name="search-bar" class="search" placeholder="GitHub user name" v-model="queryText" v-on:keypress.enter="getData(null, 'init')">
+        <button class="search-button" type="button" v-on:click="getData(null, 'init')">Search</button>
       </div>
     </section>
     <section class="results">
@@ -14,21 +14,21 @@
       <h2 v-show="userCount > 0">Found {{ userCount }} results!</h2>
       <div class="results-wrapper">
         <div class="result-item" v-for="person in users" :key="person.cursor">
-          <img :src="person.node.avatarUrl" class="result-img">
+          <img :src="person.avatarUrl" class="result-img">
           <div class="result-names">
-            <a :href="person.node.url" class="username" target="_blank">{{ person.node.login }}</a>
-            <a :href="person.node.url" v-if="person.node.name" class="display" target="_blank">{{ person.node.name }}</a>
+            <a :href="person.url" class="username" target="_blank">{{ person.login }}</a>
+            <a :href="person.url" v-if="person.name" class="display" target="_blank">{{ person.name }}</a>
           </div>
           <div class="result-info">
             <div class="result-tidbits">  
-              <p class="hireable" v-if="person.node.isHireable"><img src="../static/icons/exclamation-circle.svg" class="icons-hire"> For Hire!</p>
-              <p class="followers"><img src="../static/icons/user.svg" class="icons-tidbits"> {{ person.node.followers.totalCount }} followers</p>
-              <p class="starred"><img src="../static/icons/star.svg" class="icons-tidbits"> {{ person.node.starredRepositories.totalCount }} starred repos</p>
+              <p class="hireable" v-if="person.isHireable"><img src="../static/icons/exclamation-circle.svg" class="icons-hire"> For Hire!</p>
+              <p class="followers"><img src="../static/icons/user.svg" class="icons-tidbits"> {{ person.followers.totalCount }} followers</p>
+              <p class="starred"><img src="../static/icons/star.svg" class="icons-tidbits"> {{ person.starredRepositories.totalCount }} starred repos</p>
             </div>
-            <p class="result-bio">{{ person.node.bio }}</p>
+            <p class="result-bio">{{ person.bio }}</p>
           </div>
-          <div class="result-repos" v-if="person.node.pinnedItems.edges.length > 0">
-            <div class="repo-wrapper" v-for="(repo, index) in person.node.pinnedItems.edges" :key="index">
+          <div class="result-repos" v-if="person.pinnedItems.edges.length > 0">
+            <div class="repo-wrapper" v-for="(repo, index) in person.pinnedItems.edges" :key="index">
               <a :href="repo.node.url" class="repo-name" target="_blank">{{ repo.node.name }}</a>
               <p class="repo-desc">{{ repo.node.description }}</p>
               <ul class="repo-lang" v-if="repo.node.languages && repo.node.languages.edges.length > 0">
@@ -40,8 +40,8 @@
         </div>
       </div>
       <div class="pagination" v-if="pageInfo.prev || pageInfo.next">
-        <button type="button" class="prev-arrow" v-if="pageInfo.prev" v-on:click="loadPage('backward')"><img src="../static/icons/chevron-left.svg" class="icons-arrows"> Prev Page</button>
-        <button type="button" class="next-arrow" v-if="pageInfo.next" v-on:click="loadPage('forward')">Next Page <img src="../static/icons/chevron-right.svg" class="icons-arrows"></button>
+        <button type="button" class="prev-arrow" v-if="pageInfo.prev" v-on:click="getData(pageInfo.firstUser, 'backward')"><img src="../static/icons/chevron-left.svg" class="icons-arrows"> Prev Page</button>
+        <button type="button" class="next-arrow" v-if="pageInfo.next" v-on:click="getData(pageInfo.lastUser, 'forward')">Next Page <img src="../static/icons/chevron-right.svg" class="icons-arrows"></button>
       </div>
     </section>
   </div>
@@ -54,6 +54,7 @@ export default Vue.extend({
   data() {
     return {
       queryText: '',
+      queryType: '',
       users: {},
       noUsers: false,
       userCount: 0,
@@ -64,37 +65,88 @@ export default Vue.extend({
         firstUser: '',
         lastUser: '',
       },
-      testData: {},
     }
   },
   methods: {
-    // this call is for initial searches - will get the first 9 results for the query
-    async getInitData() {
-      this.loading = true;
-
-      this.testData = await fetch(`.netlify/functions/githubApiCall?q=${this.queryText}`);
+    // clear any previously set data, to ensure we don't get mixed up info
+    clearData() {
+      this.queryType = '';
+      this.users = {};
+      this.noUsers = false;
+      this.userCount = 0;
+      this.pageInfo = {
+        prev: false,
+        next: false,
+        firstUser: '',
+        lastUser: '',
+      };
     },
-    getPaginatedData(cursor, direction) {
-      // this call works for prev/next searches, providing the cursor to grab the next or prev 9 results
+    // call GH GraphQL API to get user data
+    async getData(cursor, direction) {
       this.loading = true;
+      this.clearData();
+
       //scrolls user to top of page
       window.scroll({
         top: 0,
         left: 0,
         behavior: 'smooth'
       });
-      // if prev clicked, gets last 9 results before the first cursor
+
+      // determine which type of query params we need to send our gql query
+      this.queryType = this.setQueryType(cursor, direction);
+
+      // make the actual API call
+      const userData = await fetch(`.netlify/functions/githubApiCall?q=${this.queryText}&type=${this.queryType}`).then(res => res.json());
+
+      // set the number of users
+      this.userCount = userData.overview.userCount;
       
+      // logic so if no results return, user will see informational data
+      if (this.userCount === 0) {
+        this.noUsers = true;
+        return;
+      }
+
+      // set info for cursors and pagination
+      const pageData = userData.overview.pageInfo;
+      this.pageInfo.prev = pageData.hasPreviousPage;
+      this.pageInfo.next = pageData.hasNextPage;
+      this.pageInfo.firstUser = pageData.startCursor;
+      this.pageInfo.lastUser = pageData.endCursor;
+
+      // filtering user data to remove anyone who has a name but nothing else
+      const results = userData.usersInfo;
+      console.log(results)
+      const legitUsers = results.filter(user => {
+        return Object.keys(user).length > 0;
+      });
+
+      // update userCount, remove loading state, then show users
+      this.userCount -= legitUsers.length;
+      this.loading = false;
+      this.users = legitUsers;
     },
-    loadPage(direction) {
-      // checks which direction we're going and sending in the correct cursor and direction
-      if (direction === "backward") {
-        this.getPaginatedData(this.pageInfo.firstUser, "backward")
+    setQueryType(cursor, direction) {   
+      // depending on our directional navigation, return the proper query variables we need
+      const firstQuery = `first: 9`;
+      const backQuery = `last: 9, before: "${cursor}"`;
+      const fwdQuery = `first: 9, after: "${cursor}"`;
+
+      switch(direction) {
+        case "init":
+          return firstQuery;
+          break;
+        case "forward":
+          return fwdQuery;
+          break;
+        case "backward":
+          return backQuery;
+          break;
+        default:
+          return firstQuery;
       }
-      else {
-        this.getPaginatedData(this.pageInfo.lastUser, "forward")
-      }
-    }
+    },
   }
 })
 </script>
