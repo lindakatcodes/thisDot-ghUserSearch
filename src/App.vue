6c95c8ccb9d1867a9b8e68cf80f11a85c9a80b82
Lindakat <lindakt16@gmail.com>
2021-04-22 18:45:46 -0500

    Style changes updated, media queries added for small and medium screens

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <template>
-  <div class="main-wrapper">
+  <main class="main-wrapper">
     <h1>GitHub User Search</h1>
-    <section class="search-section">
-      <Label for="search-bar" class="search-bar-label"> <img src="../static/icons/search.svg" class="icons"> Who would you like to search for?</Label>
-      <div class="search-wrapper">
-        <input type="text" name="search-bar" class="search" placeholder="GitHub user name" v-model="queryText" v-on:keypress.enter="getData(null, 'init')">
-        <button class="search-button" type="button" v-on:click="getData(null, 'init')">Search</button>
-      </div>
+    <section class="search-wrapper">
+        <Label for="search-bar" class="search-label"> <img src="../static/icons/search.svg" class="icons"> Who would you like to search for?</Label>
+        <input type="text" name="search-bar" class="search-input" placeholder="GitHub user name" v-model="queryText" v-on:keypress.enter="getData(null, 'first')">
+        <button class="search-button" type="button" v-on:click="getData(null, 'first')">Search</button>
     </section>
     <section class="results">
       <p class="loading" v-show="loading">Loading results...</p>
@@ -15,12 +13,12 @@
       <div class="results-wrapper">
         <UserCard v-for="person in users" :key="person.cursor" :person="person" />
       </div>
-      <div class="pagination" v-if="pageInfo.prev || pageInfo.next">
-        <button type="button" class="prev-arrow" v-if="pageInfo.prev" v-on:click="getData(pageInfo.firstUser, 'backward')"><img src="../static/icons/chevron-left.svg" class="icons-arrows"> Prev Page</button>
-        <button type="button" class="next-arrow" v-if="pageInfo.next" v-on:click="getData(pageInfo.lastUser, 'forward')">Next Page <img src="../static/icons/chevron-right.svg" class="icons-arrows"></button>
-      </div>
     </section>
-  </div>
+    <section class="navigation" v-if="pageInfo.prev || pageInfo.next">
+      <button type="button" class="prev-arrow" v-if="pageInfo.prev" v-on:click="getData(pageInfo.firstUser, 'backward')"><img src="../static/icons/chevron-left.svg" class="icons icons-arrows"> Prev Page</button>
+      <button type="button" class="next-arrow" v-if="pageInfo.next" v-on:click="getData(pageInfo.lastUser, 'forward')">Next Page <img src="../static/icons/chevron-right.svg" class="icons icons-arrows"></button>
+    </section>
+  </main>
 </template>
 
 <script>
@@ -61,12 +59,12 @@ export default Vue.extend({
         lastUser: '',
       };
     },
-    // call GH GraphQL API to get user data
+    // call GH GraphQL API to get user data & update state
     async getData(cursor, direction) {
       this.loading = true;
 
       //if this is a paginated call, scroll user to top of page
-      if (direction !== 'init') {
+      if (direction !== 'first') {
         window.scroll({
           top: 0,
         left: 0,
@@ -75,11 +73,11 @@ export default Vue.extend({
       }
 
       // if this is a first call, clear any previous data
-      if (direction === 'init') {
+      if (direction === 'first') {
         this.clearData();
       }
 
-      // determine which type of query params we need to send our gql query
+      // determine which type of query params we need to send our gql query - is this the first search, or are we paginating forwards or backwards?
       this.queryType = this.setQueryType(cursor, direction);
 
       // make the actual API call
@@ -103,7 +101,6 @@ export default Vue.extend({
 
       // filtering user data to remove anyone who has a name but nothing else
       const results = userData.usersInfo;
-      console.log(results)
       const legitUsers = results.filter(user => {
         return Object.keys(user).length > 0;
       });
@@ -113,14 +110,14 @@ export default Vue.extend({
       this.loading = false;
       this.users = legitUsers;
     },
+    // determines what needs to be added to the search query to get the right results
     setQueryType(cursor, direction) {   
-      // depending on our directional navigation, return the proper query variables we need
       const firstQuery = `first: 9`;
       const backQuery = `last: 9, before: "${cursor}"`;
       const fwdQuery = `first: 9, after: "${cursor}"`;
 
       switch(direction) {
-        case "init":
+        case "first":
           return firstQuery;
           break;
         case "forward":
--- src/components/userCard.vue
+++ src/components/userCard.vue
@@ -1,26 +1,26 @@
 <template>
-  <div class="result-item">
-    <img :src="person.avatarUrl" class="result-img">
-    <div class="result-names">
+  <div class="person-wrapper">
+    <img :src="person.avatarUrl" class="person-img">
+    <div class="person-names">
       <a :href="person.url" class="username" target="_blank">{{ person.login }}</a>
-      <a :href="person.url" v-if="person.name" class="display" target="_blank">{{ person.name }}</a>
+      <a :href="person.url" v-if="person.name" class="display-name" target="_blank">{{ person.name }}</a>
     </div>
-    <div class="result-info">
-      <div class="result-tidbits">  
-        <p class="hireable" v-if="person.isHireable"><img src="../../static/icons/exclamation-circle.svg" class="icons-hire"> For Hire!</p>
-        <p class="followers"><img src="../../static/icons/user.svg" class="icons-tidbits"> {{ person.followers.totalCount }} followers</p>
-        <p class="starred"><img src="../../static/icons/star.svg" class="icons-tidbits"> {{ person.starredRepositories.totalCount }} starred repos</p>
+    <div class="person-about">
+      <div class="person-stats">  
+        <p class="hireable" v-if="person.isHireable"><img src="../../static/icons/exclamation-circle.svg" class="icons icons-hire"> For Hire!</p>
+        <p class="followers"><img src="../../static/icons/user.svg" class="icons icons-follow"> <span class="count">{{ person.followers.totalCount }}</span> <span class="count-text">followers</span></p>
+        <p class="starred"><img src="../../static/icons/star.svg" class="icons icons-star"> <span class="count">{{ person.starredRepositories.totalCount }}</span> <span class="count-text"> starred repos</span></p>
       </div>
-      <p class="result-bio">{{ person.bio }}</p>
+      <p class="person-bio">{{ person.bio }}</p>
     </div>
-    <div class="result-repos" v-if="person.pinnedItems.edges.length > 0">
+    <div class="person-repos" v-if="person.pinnedItems.edges.length > 0">
       <div class="repo-wrapper" v-for="(repo, index) in person.pinnedItems.edges" :key="index">
+        <hr>
         <a :href="repo.node.url" class="repo-name" target="_blank">{{ repo.node.name }}</a>
         <p class="repo-desc">{{ repo.node.description }}</p>
         <ul class="repo-lang" v-if="repo.node.languages && repo.node.languages.edges.length > 0">
           <li v-for="(lang, index) in repo.node.languages.edges" :key="index">{{ lang.node.name }}</li>
         </ul>
-        <hr>
       </div>
     </div>
   </div>
@@ -35,5 +35,202 @@
 </script>
 
 <style scoped>
+  /* Card wrapper */
+  .person-wrapper {
+    display: flex;
+    flex-flow: column;
+    /* justify-content: center; */
+    /* flex-wrap: wrap; */
+    width: 95%;
+    margin: 0 auto 4%;
+    padding: 1%;
+    border: 1px solid var(--lightgray);
+    border-radius: 5px;
+    box-shadow: 1px 1px 6px 1px var(--lightgray);
+  }
 
+  /* Profile Photo */
+  .person-img {
+    width: 45%;
+    margin: 1% auto 0;
+    border-radius: 5px;
+  }
+
+  /* Names */
+  .person-names {
+    padding: 2%;
+    display: flex;
+    flex-flow: column;
+    align-items: center;
+  }
+
+  .username {
+    text-decoration: underline;
+    font-weight: bold;
+    color: var(--lightblue);
+  }
+
+  .username:hover {
+    color: var(--darkblue);
+  }
+
+  .display-name {
+    text-decoration: none;
+    margin-top: 1%;
+    color: var(--lightgray);
+  }
+
+  /* About */
+  .person-about {
+    padding: 0 1%;
+  }
+
+  .person-stats {
+    display: flex;
+    justify-content: space-around;
+    font-size: 0.9rem;
+  }
+
+  .person-stats p {
+    width: 30%;
+  }
+
+  .hireable {
+    padding-top: 1%;
+    font-size: 1.2rem;
+    font-weight: bold;
+    color: var(--green);
+  }
+
+  .icons-hire {
+    width: 12%;
+  }
+
+  .followers, .starred {
+    display: grid;
+    grid-template-areas: 
+    'icon count' 'text text';
+    place-items: center;
+  }
+    
+  .icons-follow {
+    grid-area: icon;
+    justify-self: end;
+    width: 30%;
+  }
+
+  .icons-star {
+    grid-area: icon;
+    justify-self: end;
+    width: 33%;
+  }
+
+  .count {
+    grid-area: count;
+    justify-self: start;
+    margin-left: 3%;
+    font-size: 1rem;
+  }
+
+  .count-text {
+    grid-area: text;
+  }
+
+  .person-bio {
+    height: 65px;
+    margin: 1%;
+    text-align: center;
+  }
+
+  /* Repositories */
+  .person-repos {
+    margin-top: 1%;
+  }
+
+  .repo-wrapper {
+    margin: 0 2%;
+  }
+
+  .repo-name {
+    margin-top: 1%;
+    font-weight: bold;
+    word-break: break-all;
+    color: var(--lightblue);
+  }
+
+  .repo-name:hover {
+    color: var(--darkblue);
+  }
+
+  .repo-desc {
+    margin: 0;
+  }
+
+  .repo-lang {
+    display: flex;
+    justify-content: center;
+    margin: 2% 0 4%;
+    padding: 0;
+    font-size: 0.9rem;
+  }
+
+  .repo-lang li {
+    padding: 0.5% 4%;
+    font-weight: bold;
+    color: var(--green);
+    list-style: none;
+    border-radius: 5px;
+  }
+
+  hr {
+    width: 75%;
+    color: var(--lightgray);
+  }
+
+  /* Small screens */
+  @media screen and (max-width: 620px) {
+    .person-bio {
+      height: auto;
+      margin: 1% 2%;
+    }
+
+    .repo-wrapper {
+      margin: 0 3%;
+    }
+
+    .repo-lang li {
+      font-size: 1rem;
+    }
+  }
+
+  /* Medium screens */
+  @media screen and (min-width: 620px) and (max-width: 1280px) {
+    .person-about {
+      padding: 0 2%;
+    }
+
+    .hireable {
+      font-size: 1.1rem;
+    }
+
+    .icons-hire {
+      width: 14%;
+    }
+
+    .icons-follow {
+      width: 37%;
+    }
+
+    .icons-star {
+      width: 39%;
+    }
+
+    .person-bio {
+      margin: 1% 2%;
+    }
+
+    .repo-wrapper {
+      margin: 0 3%;
+    }
+  }
 </style>
